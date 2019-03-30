b00889617c47ac75837ef5a2d0f8dc4ac814b0c8
shakhawat21 <48868371+shakhawat21@users.noreply.github.com>
2019-03-30 11:25:50 -0400

    Add files via upload

---
+++ String_Assignment_mar_28.java
@@ -0,0 +1,161 @@
+package assignment;
+
+//public class Assignment {
+//
+//	public static void main(String[] args) {
+//
+//
+//
+//	}
+//
+//}
+
+
+//1)Write a Java program to get the character at the given index
+// within the String. 
+
+//public class Assignment {
+//public static void main(String[] args) {
+//String str = "WORLDWIDE";
+//char getChar = str.charAt(5);
+//System.out.println("5th Char is : "+getChar); // S
+//
+//}
+//}
+
+
+//2.Write a Java program to get the character (Unicode code
+// point) at the given index within the String.
+
+//public class Assignment {
+//public static void main(String[] args) {
+//String str = "Earth is round";
+//int UniC = str.codePointAt(4);
+//int UniCodB=str.codePointBefore(4);
+//int UniCodC=str.codePointCount(1, 2);
+//System.out.println(" Unicode code point at 4 is : "+UniC); 
+////System.out.println(" Unicode code point at 4 is : "+UniCodB);
+////System.out.println(" Unicode code point count (1,2) is :"+ UniCodC);
+//}
+//}
+
+
+
+
+//  3) Write a java program to count a character of a String.
+//
+
+//public class Assignment {
+//public static void main(String[] args) {
+//String CountChar = "shakhawat2100@gmail.com";
+//System.out.println("Total Character is : "+ CountChar.length());
+//
+//}
+//}
+
+//4)Write a java program to compare two strings lexicographically.
+
+//public class Assignment {
+//public static void main(String[] args) {
+//String  str1 = "abcd"; // Unicode 97 -122
+//String str2 = "ABCD"; // Unicode 65 -90
+//
+//int compare1 = str1.compareTo(str2);
+//
+//int COMPARE2 =str2.compareTo(str1);
+//
+//
+//System.out.println("Compare lowerCase to Uppercase is :" + compare1);
+//
+//System.out.println("Compare Uppercase TO lowerCase  is :" + COMPARE2);
+//}
+//}
+
+
+
+//5) Write a Java program to concatenate a given string to
+// the end of another string. 
+
+//public class Assignment {
+//public static void main(String[] args) {
+//String str1 = "shakhawat";
+//String str2 = " 100";
+//System.out.println(str1 + str2);
+//
+//}
+//}
+
+
+//6) Write a Java program to test if a given string
+// contains the specified sequence of char values.
+
+
+
+//public class Assignment {
+//public static void main(String[] args) {
+//String str = "100pinktoys";
+
+//System.out.println("Example 1 : "+str.contains("100p"));
+//System.out.println("Example 2 :"+str.contains("Pink"));
+
+//}
+//}
+
+
+
+//7) Write a java program to
+// get the length of a given string.
+
+
+//public class Assignment {
+//public static void main(String[] args) {
+//String CountChar = "shakhawat2100@gmail.com";
+//System.out.println("Total Character is : "+ CountChar.length());
+//
+//}
+//}
+
+
+
+
+//  8)Write a Java program to convert all the
+// characters in a string to lowercase. 
+
+
+//public class Assignment {
+//public static void main(String[] args) {
+//
+//String str = "SELENIUM_Class";
+//
+//System.out.println(str.toLowerCase()); // selenium
+//
+//
+//}
+//}
+
+
+// 9) Write a Java program to convert all
+// the characters in a string to uppercase 
+
+//public class Assignment {
+//public static void main(String[] args) {
+//	String str = "seleNIUM_Class";
+//
+//	System.out.println(str.toUpperCase()); // selenium
+//
+//}
+//}
+
+
+//10) Write a program to reverse a
+// string
+
+//public class Assignment {
+//public static void main(String[] args) {
+//	StringBuilder s2 = new StringBuilder(" Sunny day beach day");
+//		System.out.println(s2);
+//	s2.reverse();
+//		System.out.println("StringBuilder : "+ s2); // selenium
+//
+//}
+//}
